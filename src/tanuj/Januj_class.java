1b9fa222f05a97fa6bd5cc45217a0adaa15a9f02
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-04-08 21:46:50 +0530

    tanuj

---
+++ src/tanuj/Januj_class.java
@@ -0,0 +1,14 @@
+package tanuj;
+
+public class Januj_class {
+    public static void main(String[] args) {
+        System.out.println("today is the first day of tanuj");
+
+        int age;
+        String city;
+
+        int age1= 17;
+        System.out.println("he is not eligible for voting");
+
+    }
+}
